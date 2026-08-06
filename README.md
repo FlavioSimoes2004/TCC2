fcbe7447726a81b8fd4df78457070215fcc7305e
flavio <flaviosimoes@prottus.com.br>
2026-08-06 09:09:34 -0300

    instalacao e setup do mininet

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # TCC2
+# INSTALAÇÕES e SETUP
+```bash
+sudo dnf update -y
+sudo dnf install -y git openvswitch net-tools python3
+sudo dnf install -y telnet
+```
+
+- SETUP MININET:
+```bash
+git clone https://github.com/mininet/mininet
+cd mininet
+mininet/util/install.sh
+
+sudo python3 setup.py install
+sudo ln -s /usr/local/bin/mn /usr/bin/mn
+
+sudo make mnexec
+sudo cp mnexec /usr/local/bin/
+```
---
+++ star_topology.py
@@ -0,0 +1,47 @@
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import Controller
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+
+class StarTopo(Topo):
+    """Star topology with one central switch and n hosts."""
+
+    def build(self, n=4):
+        """Build the topology.
+        n: number of hosts
+        """
+        # Add the central switch
+        switch = self.addSwitch('s1')
+
+        # Add n hosts and connect them to the switch
+        for i in range(1, n + 1):
+            host_name = f'h{i}'
+            host = self.addHost(host_name)
+            # Add link between host and switch
+            self.addLink(host, switch)
+
+def run():
+    "Create and test a simple star network"
+    # Set the log level to info to see what's happening
+    setLogLevel('info')
+
+    info("*** Creating network\n")
+    # Instantiate the topology with 5 hosts
+    topo = StarTopo(n=5)
+    
+    # Create the network with the default controller
+    net = Mininet(topo=topo, controller=Controller)
+
+    info("*** Starting network\n")
+    net.start()
+
+    info("*** Running CLI\n")
+    # Start the Mininet Command Line Interface
+    CLI(net)
+
+    info("*** Stopping network\n")
+    net.stop()
+
+if __name__ == '__main__':
+    run()
