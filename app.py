4322a5639a05201123db2c2f5111174a807e1177
flavio <flaviosimoes@prottus.com.br>
2026-08-07 08:35:07 -0300

    add captive portal com download do script

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/download_script')
+def download_script():
+    file_path = '/home/flavio/Documents/TCC2/script.sh'
+    return send_file(file_path, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
