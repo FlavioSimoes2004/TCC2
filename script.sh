818916d88c99140184b8ccdf87dfb1828dc8fd2d
flavio <flaviosimoes@prottus.com.br>
2026-08-11 14:25:47 -0300

    refactor: update security check logic to aggregate firewall and update statuses

--- script.sh
+++ script.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+seguro=true;
+
 # Verifica se o serviço de firewall (firewalld) está ativo
 if ! systemctl is-active --quiet firewalld; then
+    seguro=false;
     echo "false - firewall not active"
-    exit 0
 fi
 
 # Verifica se há atualizações do sistema disponíveis.
@@ -12,8 +14,11 @@ dnf check-update --quiet > /dev/null 2>&1
 update_status=$?
 
 # Se o status for 0, o sistema está atualizado. Caso contrário, não está.
-if [ $update_status -eq 0 ]; then
-    echo "true"
+if [ $seguro == true ] && [ $update_status -eq 0 ]; then
+    pass
 else
+    seguro=false;
     echo "false - update available"
 fi
+
+echo "$seguro"
